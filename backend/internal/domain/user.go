40571c8a38082e4054bd7c2f1c3168f3c317f5d0
KokiYoshikawa <yskw.kk.git@gmail.com>
2026-07-04 01:43:37 +0900

    ユーザー登録画面_usersのentity作成

---
+++ backend/internal/domain/user.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type UserRole string
+
+const (
+	UserRoleUser  UserRole = "user"
+	UserRoleAdmin UserRole = "admin"
+)
+
+type User struct {
+	ID           int64
+	Name         string
+	Email        string
+	PasswordHash string
+	Role         UserRole
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+}
